c494a25250a8d7fd374004a76cc776a44f37cb3f
parsa <parsa@beastbrain.org>
2023-08-27 14:32:25 +0330

    tls support for usage added

--- foreignusage/usage.go
+++ foreignusage/usage.go
@@ -1,9 +1,20 @@
 package foreignusage
 
-import "github.com/Kawaii-Konnections-KK-Limited/subscription/api"
+import (
+	"log"
 
-func InitService(gupFunc func(token *string) *[]string, vtFunc func(token *string) bool) {
+	"github.com/Kawaii-Konnections-KK-Limited/subscription/api"
+)
 
-	api.InitRoutes(gupFunc, vtFunc).Run()
+func InitService(gupFunc func(token *string) *[]string, vtFunc func(token *string) bool, addr, certFile, keyFile *string) {
+
+	if addr != nil && certFile != nil && keyFile != nil {
+		log.Println("running with tls")
+		api.InitRoutes(gupFunc, vtFunc).RunTLS(*addr, *certFile, *keyFile)
+	} else {
+		log.Println("running without tls")
+
+		api.InitRoutes(gupFunc, vtFunc).Run()
+	}
 
 }
